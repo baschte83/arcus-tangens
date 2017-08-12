b6bc97c9c49f521dda52ac3d75fb4f064d0966cd
Sebastian Baumann <bauman21@hm.edu>
2017-08-12 16:07:43 +0200

    Finishing ArcusTangens.java file with additional javadoc comments and translation of existing comments to english.

--- src/ArcusTangens.java
+++ src/ArcusTangens.java
@@ -1,43 +1,51 @@
-/* Organisation: Hochschule München, Fakultät 07
- * Project: Praktikum Softwareentwicklung 1 Frau Prof. Dr. Hammerschall, WS 2016/2017
- * Author: Sebastian Baumann, bauman21@hm.edu
+/**
+ * Organisation: University of applied sciences Munich, faculty 07
+ * Project: lab to class software developement 1 Mrs. Prof. Dr. Hammerschall, WS 2016/2017
+ * Author: Sebastian Baumann, sebastian_baumann@gmx.net
  * Study group: IF3A
  * Date: 26. Oktober 2016
- * Purpose: Lösung "Praktikumsinheit 02", Aufgabe Arcus Tangens
- * Software: Oracle Java SE JDK 1.8.0_101, Windows 10 x86_64
- * Hardware: Intel Core i5-6300 @ 2.40 GHz 2.50 GHz, 2 Cores, 4096 MB RAM
+ * Purpose: solution to lab Arcus Tangens
  */
-
 public class ArcusTangens {
 
-    // Konstante für Rechengenauigkeit
+    /**
+     * Constant value EPSILON which stores the accuracy with which the arctangent is computed.
+     * Constant value THREE is just a workaround to not use a "magic number" in the formula to compute the series.
+     */
     private static final double EPSILON = 1E-16;
+    private static final double THREE = 3.0;
 
+    /**
+     * Main method to compute the arctangent.
+     * @param args - variable number of arguments handeld over from the terminal.
+     */
     public static void main(String[] args) {
 
-        // Definition der benötigten Variablen
-        double x = Double.parseDouble(args[0]); 			// x-Wert des Benutzers, von dem der Arcus Tangens berechnet werden soll
-        double sn = x;										// Variable zum speichern der berechneten Partialsumme
-        double an = x;										// Variable zum speichern von einem Summanden an
-        double anPlusOne = x;								// Variable zum speichern des nächsten Summanden an+1, berechnet an+1 ausgehend von an (rekursiv)
-        int n = 1;									// Zählvariable, wird in der Schleife benutzt
-        int twoN;									// Variable zum Speichern des Wertes von (2n), da dieser Wert in der Berechnung von sn oft benötigt wird
+        // Definition of all used variable values in the formula.
+        double x = Double.parseDouble(args[0]); 			// value the user wants to compute the arctangent of
+        double sn = x;										// variable to store the computed partial sum
+        double an;										    // variable to store the computed operand an
+        double anPlusOne = x;								// variable to store the operand an+1 (recursive with an as the beginning)
+        int n = 1;									        // counter used in the while loop
+        int twoN;									        // variable to store the value of the operation 2*n used in the formula
 
 
-        // Berechnung des Arcus Tangens mittels einer while-Schleife
+        // While loop to compute the arctangent of x with an accuracy of EPSILON.
         while (Math.abs(anPlusOne) > EPSILON) {
 
             twoN = 2 * n;
             an = Math.pow(-1, n) * ((Math.pow(x, twoN + 1) / (twoN + 1)));
-            anPlusOne = (-((twoN + 1.0) / (twoN + 3.0)) * x * x) * an;
+            anPlusOne = (-((twoN + 1.0) / (twoN + THREE)) * x * x) * an;
             sn += an;
             n++;
 
         }
 
-        // Ausgabe des von meinem Programm berechneten Wertes Arcus Tangens von x
+
+        // Output of the arctangent of x computed with my algorithm.
         System.out.println(sn);
-        // Ausgabe des von Math.atan berechneten Vergleichswerts des Arcus Tangens von x
+
+        // Output of the arctangent of x computed with the java MATH library function Math.atan(x).
         System.out.println(Math.atan(x));
 
     }
